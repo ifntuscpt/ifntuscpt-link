4aa7d8aa42122d1fd336630dca50f7abf6cb8bcd
ifentius ciputra <95.ifentiusciputra@gmail.com>
2023-06-13 19:00:28 +0800

    Adding portofolio link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BsInstagram } from "react-icons/bs";
-import { BsFacebook } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
 import { BsSpotify } from "react-icons/bs";
 import { FiYoutube } from "react-icons/fi";
 import { FaGithub } from "react-icons/fa";
+import { FaGoogleDrive } from "react-icons/fa";
+import { MdOutgoingMail } from "react-icons/md";
 import { SiIssuu } from "react-icons/si";
 import TextLoop from "react-text-loop";
 import Typist from 'react-typist';
@@ -30,7 +31,7 @@ function App() {
 						<img src="/profile.png" className="avatar" alt="avatar" />
 						<div className="bg_content rd_12 p_8">
 							<h1>Ifentius Ciputra</h1>
-							<p className='mt-16'>Undergraduate Information Technology Student</p>
+							<p className='mt-16'>Information Technology Student</p>
 							<Typist cursor={config_cursor}>
 							at Udayana University
 							</Typist>
@@ -48,21 +49,34 @@ function App() {
 						</div>
 					</div>
 					<div className="mt-16">
+						<a className="btn_action bg_content" href="mailto:95.ifentiusciputra@gmail.com">
+							<MdOutgoingMail />
+							<span>Have a Cool Project?</span>
+						</a>
+					</div>
+					<div className="mt-16">
+						<a className="btn_action bg_content" href="https://drive.google.com/file/d/11lATY6vCpSmTL4P_BmEziWIA3nCTq_ov/view">
+							<FaGoogleDrive />
+							<span>UI/UX Portofolio</span>
+						</a>
+					</div>
+					</div>
+					<div className="mt-16">
+						<a className="btn_action bg_content" href="https://issuu.com/ifntuscpt/docs/uiux_grapich_designer_portfolio-ife_c8441f8618f0f7">
+							<SiIssuu />
+							<span>Additional Portfolio</span>
+						</a>
+					</div>
+					<div className="mt-16">
 						<a className="btn_action bg_content" href="https://www.linkedin.com/in/ifntuscpt">
 							<BsLinkedin />
 							<span>Linkedin</span>
 						</a>
 					</div>
 					<div className="mt-16">
-						<a className="btn_action bg_content" href="https://www.instagram.com/ifntuscpt">
+						<a className="btn_action bg_content" href="https://www.instagram.com/ui.fent">
 							<BsInstagram />
 							<span>Instagram</span>
-						</a>
-					</div>
-					<div className="mt-16">
-						<a className="btn_action bg_content" href="https://www.facebook.com/ifntus">
-							<BsFacebook />
-							<span>Facebook</span>
 						</a>
 					</div>
 					<div className="mt-16">
@@ -70,17 +84,10 @@ function App() {
 							<FaGithub />
 							<span>Github</span>
 						</a>
-					</div>
-					<div className="mt-16">
-						<a className="btn_action bg_content" href="https://issuu.com/ifntuscpt/docs/uiux_grapich_designer_portfolio-ife_c8441f8618f0f7">
-							<SiIssuu />
-							<span>Portofolio Issuu</span>
-						</a>
-					</div>
 					<div className="mt-16">
 						<a className="btn_action bg_content" href="https://open.spotify.com/show/0T9XaW7xTjxEjqIiBtqC99?si=cca279f6a8434023">
 							<BsSpotify />
-							<span>Spotify Podcast</span>
+							<span>My Podcast</span>
 						</a>
 					</div>
 					<div className="mt-16">
